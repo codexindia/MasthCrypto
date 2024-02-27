51e24d6781e0009e2997f569416ed0bc7949c501
codexindia <sudiptobain8@gmail.com>
2024-02-27 16:43:02 +0530

    update

--- app/Filament/Pages/Settings.php
+++ app/Filament/Pages/Settings.php
@@ -4,9 +4,7 @@ namespace App\Filament\Pages;
 
 use Filament\Actions\Action;
 use Filament\Forms\Components\{Group, Section};
-
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\{Select,Radio,TextInput};
 use Filament\Forms\Form;
 use Filament\Notifications\Notification;
 use Illuminate\Contracts\View\View;
@@ -17,7 +15,7 @@ use Filament\Pages\Page;
 class Settings extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-cog';
-
+public $json;
     protected static string $view = 'filament.pages.settings';
     public function mount(): void
     {
@@ -28,44 +26,49 @@ class Settings extends Page
         return $form
             ->schema([
                 Group::make()->schema([
-                    Section::make()->schema([
+                    Section::make('General options')->schema([
                         Select::make('maintenance_mode')
-                        ->required()->options([
-                            '1' => 'Active',
-                            '2' => 'Deactive'
-                        ]),
-                        TextInput::make('name')
-                            ->required(),
-                        TextInput::make('name')
-                            ->required(),
-                        TextInput::make('name')
-                            ->required(),
-                        TextInput::make('name')
-                            ->required(),
+                            ->required()->options([
+                                '1' => 'Enable',
+                                '0' => 'Disable'
+                            ]),
+                        Select::make('force_update')
+                            ->required()->options([
+                                '1' => 'Enable',
+                                '0' => 'Disable'
+                            ]),
+                            Select::make('ad_network')
+                            ->required()->options([
+                                'admob' => 'Admob',
+                               
+                            ]),
+                            Radio::make('mining_function')
+                            ->label('Mining Function')
+                            ->boolean()
+                            ->inline()
+                            ->default(1)
+                            ->inlineLabel(false),
+                        
                     ])->columns(2)
 
-                    ]),
-                    Group::make()->schema([
-                        Section::make()->schema([
-                            Select::make('maintenance_mode')
-                            ->required()->options([
-                                '1' => 'Active',
-                                '2' => 'Deactive'
-                            ]),
-                            TextInput::make('name')
-                                ->required(),
-                            TextInput::make('name')
-                                ->required(),
-                            TextInput::make('name')
-                                ->required(),
-                            TextInput::make('name')
-                                ->required(),
-                        ])->columns(2)
-    
-                    ])
+                ]),
+                Group::make()->schema([
+                    Section::make('Important Settings')->schema([
+                       
+                        TextInput::make('referral_coin')
+                            ->required()->integer(),
+                        TextInput::make('joining_coin')
+                            ->required()->integer(),
+                        TextInput::make('sm_country_t_charge')
+                            ->required()->label('Same Country Transfer Charges')->integer(),
+                        TextInput::make('diff_country_t_charge')
+                            ->required()->label('Other Country Transfer Charges')->integer(),
+                    ])->columns(2)
 
-            ])
-            ->statePath('data');
+                ])
+
+            ])->columns(2);
+           
     }
     protected function getFormActions(): array
     {
@@ -75,10 +78,10 @@ class Settings extends Page
                 ->submit('save'),
         ];
     }
-    public function save(): void
+    public function save()
     {
-
-
+        
+     
         Notification::make()
             ->success()
             ->title("Changes Saved SuccessFully")
