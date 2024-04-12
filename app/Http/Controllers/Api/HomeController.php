2648b710dee433c289eb3c345fb63d535f68a9d8
codexindia <sudiptobain8@gmail.com>
2024-04-12 20:08:50 +0530

    hb

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -29,7 +29,7 @@ class HomeController extends Controller
             'status' => true,
             'valuation' => array(
                 'currency' => 'USD',
-                'rate' => 1,
+                'rate' => 700,
             ),
             'active_miners' => $data['active_miners']->count(),
             'total_miners' => $data['active_miners']->count(),
