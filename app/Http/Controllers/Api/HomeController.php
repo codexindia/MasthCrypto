8a23f0ed044fef9d40d08bbe1196dd19578933a0
codexindia <sudiptobain8@gmail.com>
2024-04-12 11:47:23 +0530

    df

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -17,11 +17,16 @@ class HomeController extends Controller
        
         return response()->json([
             'status' => true,
+            'valuation' => array(
+                'currency' => 'USD',
+                'rate' => 0.77,
+            ),
             'active_miners' => $data['active_miners']->count(),
             'total_miners' => $data['active_miners']->count(),
            
             'total_live_mining' => MiningSession::sum('coin'),
-            'total_remote_mining' => (float) 0,
+            'total_remote_mining' => (float) 500,
+
         ]);
     }
 }
