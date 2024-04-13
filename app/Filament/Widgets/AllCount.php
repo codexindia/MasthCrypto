c7b91a42a539f9eece135677dea7d81f2e72933b
codexindia <sudiptobain8@gmail.com>
2024-04-13 19:27:26 +0530

    asda

--- app/Filament/Widgets/AllCount.php
+++ app/Filament/Widgets/AllCount.php
@@ -7,39 +7,39 @@ use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use App\Models\User;
 use App\Models\MiningSession;
+
 class AllCount extends BaseWidget
 {
     protected function getStats(): array
     {
         $user_count = User::count();
         $activeUserCount = MiningSession::where('created_at', '>=', now()->subHours(24))
-        ->distinct('user_id')
-        ->count('user_id');
+            ->distinct('user_id')
+            ->count('user_id');
         $TotalClicks = MiningSession::whereDate('created_at', Carbon::today())
-        ->count();
+            ->selectRaw('SUM(coin) as total_coin, COUNT(*) as count')->first();
         return [
-            Stat::make('Total Users',$user_count)
-            ->description('increase in Users')
-            ->descriptionIcon('heroicon-m-arrow-trending-up')
-            ->color('success')
-            ->chart([2,5,2,1,5,0]),
-            
-            Stat::make('Active User ',$activeUserCount)
-            ->description('Last Mined In 24 Hours')
-            //->descriptionIcon('heroicon-m-arrow-trending-up')
-            ->color('danger'),
-           
+            Stat::make('Total Users', $user_count)
+                ->description('increase in Users')
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('success')
+                ->chart([2, 5, 2, 1, 5, 0]),
 
-            Stat::make('Total Clicks',$TotalClicks)
-            ->description('increase in Clicks')
-            ->descriptionIcon('heroicon-m-arrow-trending-up')
-            ->color('success')
-            ->chart([2,5,2,1,5,0]),
-            Stat::make('Coin Mined Today','10k')
-            ->description('increase in Mining')
-            ->descriptionIcon('heroicon-m-arrow-trending-up')
-            ->color('success')
-            ->chart([2,5,2,1,5,0])
+            Stat::make('Active User ', $activeUserCount)
+                ->description('Last Mined In 24 Hours')
+                //->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('warning'),
+
+            Stat::make('Total Clicks', $TotalClicks->count)
+                ->description('increase in Clicks')
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('success')
+                ->chart([2, 5, 2, 1, 5, 0]),
+            Stat::make('Coin Mined Today', !$TotalClicks->total_coin?0:$TotalClicks->total_coin)
+                ->description('increase in Mining')
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('success')
+                ->chart([2, 5, 2, 1, 5, 0])
         ];
     }
 }
