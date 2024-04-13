80b1c781eaa19f7eff41bf3b4b3aac2f633e9e58
codexindia <sudiptobain8@gmail.com>
2024-04-13 14:29:43 +0530

    gh

--- app/Filament/Widgets/AllCount.php
+++ app/Filament/Widgets/AllCount.php
@@ -5,11 +5,15 @@ namespace App\Filament\Widgets;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use App\Models\User;
+use App\Models\MiningSession;
 class AllCount extends BaseWidget
 {
     protected function getStats(): array
     {
         $user_count = User::count();
+        $activeUserCount = MiningSession::where('created_at', '>=', now()->subHours(24))
+        ->distinct('user_id')
+        ->count('user_id');
         return [
             Stat::make('Total Users',$user_count)
             ->description('increase in Users')
@@ -17,18 +21,18 @@ class AllCount extends BaseWidget
             ->color('success')
             ->chart([2,5,2,1,5,0]),
             
-            Stat::make('Total Users','10k')
+            Stat::make('Active User ','10k')
             ->description('increase in Users')
             ->descriptionIcon('heroicon-m-arrow-trending-up')
             ->color('success')
             ->chart([2,5,2,1,5,0]),
 
-            Stat::make('Total Users','10k')
+            Stat::make('Total Clicks','10k')
             ->description('increase in Users')
             ->descriptionIcon('heroicon-m-arrow-trending-up')
             ->color('success')
             ->chart([2,5,2,1,5,0]),
-            Stat::make('Total Users','10k')
+            Stat::make('Coin Mined Today','10k')
             ->description('increase in Users')
             ->descriptionIcon('heroicon-m-arrow-trending-up')
             ->color('success')
