a9ee1c05abf9012c623ab4b63ef9afe5e104e424
codexindia <sudiptobain8@gmail.com>
2024-04-16 23:56:56 +0530

    c

--- app/Http/helper.php
+++ app/Http/helper.php
@@ -4,6 +4,7 @@ use App\Models\Settings;
 use App\Models\CoinsTransaction;
 use App\Models\User;
 use Berkayk\OneSignal\OneSignalFacade as OneSignal;
+use Illuminate\Support\Facades\DB;
 
 if (!function_exists('get_setting')) {
     function get_setting($key, $group = 'general')
@@ -19,6 +20,7 @@ if (!function_exists('get_setting')) {
 if (!function_exists('coin_action')) {
     function coin_action(int $user_id, float $coins, $type = "debit", $description = null, $meta = [])
     {
+        DB::beginTransaction();
         $user = User::findOrFail($user_id);
         if ($user)
             $transaction = new CoinsTransaction;
@@ -26,21 +28,29 @@ if (!function_exists('coin_action')) {
         $transaction->coin = $coins;
         $transaction->transaction_type = $type;
         $transaction->description = $description;
-        $transaction->transaction_id = 'MST' .$user_id. time().rand('10','99');
+        $transaction->transaction_id = 'MST' . $user_id . time() . rand('10', '99');
         $transaction->status = 'success';
         $transaction->meta = json_encode($meta);
         if ($transaction->save()) {
             if ($type == "credit") {
-                $user->increment('coin', $coins);
-                return true;
+                if ($user->increment('coin', $coins)) {
+                    DB::commit();
+                    return true;
+                } else {
+                    DB::rollBack();
+                }
             } else {
-                $user->decrement('coin', $coins);
-                return true;
+                if ($user->decrement('coin', $coins)) {
+                    DB::commit();
+                    return true;
+                } else {
+                    DB::rollBack();
+                }
             }
         }
         return false;
     }
-    function sendpush($user_id,$text)
+    function sendpush($user_id, $text)
     {
         $params = [];
         $params['android_channel_id'] = '7fbda4a1-81c5-4eb6-9936-a80543c5c06f';
