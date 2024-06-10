e8d90aeb67d18236fd37cd88d68bf71ed25b17a7
codexindia <sudiptobain8@gmail.com>
2024-06-10 23:55:04 +0530

    sds

--- app/Filament/Widgets/RecentJoinedUser.php
+++ app/Filament/Widgets/RecentJoinedUser.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Widgets;
 
 use App\Filament\Resources\UserResource;
+use Filament\Forms\Components\Builder;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
@@ -10,6 +11,10 @@ use Filament\Widgets\TableWidget as BaseWidget;
 
 class RecentJoinedUser extends BaseWidget
 {
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->limit(5);
+    }
     public function table(Table $table): Table
     {
         return $table
