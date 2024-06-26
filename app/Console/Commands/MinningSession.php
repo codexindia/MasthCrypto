0b1f0b641e4f56665803f3b405ca9feea7a2a2cf
codexindia <sudiptobain8@gmail.com>
2024-06-26 23:47:16 +0530

    dbtransaction

--- app/Console/Commands/MinningSession.php
+++ app/Console/Commands/MinningSession.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\User;
 use Carbon\Carbon;
 
+use Exception;
 use Illuminate\Console\Command;
 use App\Models\MiningSession;
 use Illuminate\Support\Facades\DB;
@@ -37,27 +38,28 @@ class MinningSession extends Command
             $pending = MiningSession::where('status', 'running')->where('end_time', '<=', $now)->orderBy('end_time')->take(100)->get();
 
             foreach ($pending as $item) {
-                DB::beginTransaction();
-                $update = MiningSession::findOrFail($item->id);
-                $update->status = 'closed';
-                $update->save();
-                sendpush($item->user, 'Hey There ! Your Mining Session Has Ended 😨 Come Back And Start Mining Again 💰💸');
-                //push refer bonus
-                $refer_coin = get_setting('referral_coin');
-                if ($item->user->referred_by != null && $item->user->referred_by != "skiped") {
-                    $ref_user = User::where('refer_code', $item->user->referred_by)->first();
-                    coin_action($ref_user->id, $refer_coin, 'credit', "Commission Received From Your Referral User " . $item->user->name);
-                    ReferData::where([
-                        'user_id' => $ref_user->id,
-                        'referred_to' => $item->user->id,
-                    ])->first()->increment('coins_earn', $refer_coin);
-                }
-                //end refer
-                if (!coin_action($item->user_id, $item->coin, 'credit', "Coins Added For Mining Session " . $item->session_id, ['session_id' => $item->session_id])) {
-                    DB::rollBack();
-                } else {
-                    DB::commit();
-                }
+                DB::transaction(function () use ($item) {
+                    $update = MiningSession::findOrFail($item->id);
+                    $update->status = 'closed';
+                    $update->save();
+                    try {
+                        sendpush($item->user, 'Hey There ! Your Mining Session Has Ended 😨 Come Back And Start Mining Again 💰💸');
+                    } catch (\Throwable $th) {
+                       // throw new \Exception($th->getMessage());
+                    }
+                    //push refer bonus
+                    $refer_coin = get_setting('referral_coin');
+                    if ($item->user->referred_by != null && $item->user->referred_by != "skiped") {
+                        $ref_user = User::where('refer_code', $item->user->referred_by)->first();
+                        coin_action($ref_user->id, $refer_coin, 'credit', "Commission Received From Your Referral User " . $item->user->name);
+                        ReferData::where([
+                            'user_id' => $ref_user->id,
+                            'referred_to' => $item->user->id,
+                        ])->first()->increment('coins_earn', $refer_coin);
+                    }
+                    //end refer
+                    coin_action($item->user_id, $item->coin, 'credit', "Coins Added For Mining Session " . $item->session_id, ['session_id' => $item->session_id]);
+                }, 5);
             }
         } catch (\Throwable $th) {
             throw new \Exception($th->getMessage());
