f1720aee5628b276bc69602a9cab2db08ff05a40
codexindia <sudiptobain8@gmail.com>
2024-08-12 21:50:30 +0530

    asd

--- app/Http/Controllers/Api/GameZoneManager.php
+++ app/Http/Controllers/Api/GameZoneManager.php
@@ -12,17 +12,17 @@ class GameZoneManager extends Controller
     public function getGames(Request $request)
     {
         $url = url("/storage//");
-        // $gameData = GameData::where('category', $request->cat)
-        //     ->select(
-        //         DB::raw("CONCAT('$url/',thumbnail) AS thumbnail"),
-        //         'gameName',
-        //         'gameWebLink',
-        //         'rewardCoins',
-        //         'category'
-        //     )
-        //     ->orderBy('gameId', 'desc')->get();
-        $gameData = GameZone::where('category', $request->cat)
-       ->get();
+        $gameData = GameData::where('category', $request->cat)
+            ->select(
+                DB::raw("CONCAT('$url/',thumbnail) AS thumbnail"),
+                'gameName',
+                'gameWebLink',
+                'rewardCoins',
+                'category'
+            )
+            ->orderBy('gameId', 'desc')->get();
+    //     $gameData = GameZone::where('category', $request->cat)
+    //    ->get();
         return response()->json([
             'status' => true,
             'data' => $gameData,
